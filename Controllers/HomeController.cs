fb95c53b418389e6498d1985e9f5f4091f60e934
Sreemaanreddy <vindyalasreemaanreddy@gmail.com>
2025-09-08 00:52:32 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using MetricSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new MetricSystemModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(MetricSystemModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ConvertToCelsius();
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/MetricSystemModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetricSystem.Models
+{
+    public class MetricSystemModel
+    {
+        [Required(ErrorMessage = "Please enter a temperature in Fahrenheit.")]
+        [Range(-1000, 1000, ErrorMessage =
+       "Fahrenheit temperature must be between -1000 and 1000.")]
+        public decimal? Fahrenheit { get; set; }
+
+        public decimal? Celsius { get; private set; }
+
+        public decimal? ConvertToCelsius()
+        {
+            if (Fahrenheit.HasValue)
+            {
+                Celsius = (Fahrenheit.Value - 32) * 5 / 9;
+            }
+            return Celsius;
+        }
+    }
+}
